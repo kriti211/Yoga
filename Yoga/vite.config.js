2c200b51d8ddb87538438aca3c4c11dcd896d03d
Ayushi101 <ayushi.sodani@diaspark.com>
2024-12-15 16:09:31 +0530

    first commit

--- Yoga/vite.config.js
+++ Yoga/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000, // Replace 8080 with your desired port number
+    host: '0.0.0.0', // Allows the server to be accessible externally
+  },
   plugins: [react()],
 })
